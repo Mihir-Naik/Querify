ce537e956201565dc53df3c265e8bf8d5847263d
Mihir Naik <mdnaik17@gmail.com>
2017-11-09 22:10:28 -0800

    Updated navbar view

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -3,24 +3,49 @@ import { Link } from 'react-router-dom'
 
 const NavBar = (props) => {
 	return (
-		<div className='NavBar'>
-			<Link to="/">Home</Link>
+		<div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">🤔 QUERIFY</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><Link to="/">Home</Link> <span class="sr-only"></span></a>
+      </li>
 			{props.currentUser
 				? (
-					<span>
-						<p>Hi {props.currentUser.firstName} ! </p>
-						<p><Link to="/questionsIndex">Questions</Link></p>
-						<p><Link to="/blogs">Blogs</Link></p>
-						<p><Link to="/profile">Profile</Link></p>
-						<p><Link to="/logout">Log Out</Link></p>
-					</span>
-				) : (
-					<span>
-						<p><Link to="/login">Log In</Link></p>
-						<p><Link to="/signup">Sign Up</Link></p>
-					</span>
+			<div>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/questionsIndex">Questions</Link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/blogs">Blogs</Link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/profile">Profile</Link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/logout">Log Out</Link></a>
+      </li>
+			</div>
+			) : (
+			<div>
+			<li class="nav-item">
+				<a class="nav-link" href="#"><Link to="/login">Log In</Link></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#"><Link to="/signup">Sign Up</Link></a>
+			</li>
+			</div>
 				)
 			}
+    </ul>
+  	</div>
+	</nav>
 		</div>
 	)
 }
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = (props) => {
 	return (
 		<div className='Home'>
-			<h1>QUERIFY</h1>
+			{/* <h1>QUERIFY</h1> */}
+			{/* <div>
+				<img src="http://www.hd-freewallpapers.com/sunrise-hd-wallpaper/wallpaper-sunrise-image-free.jpg" alt="" width= "70%" height="80%" />
+			</div> */}
 
 		<nav className="navbar fixed-bottom navbar-light bg-light">
 			© 2017 Querify. No Rights Reserved Yet.
